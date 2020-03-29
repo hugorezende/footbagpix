e18f30a2e408cc843a13a454011531b1e161966e
Krisztián Póra <krisztian.pora@gmail.com>
2020-03-29 16:14:47 +0200

    Added Logic interfaces

---
+++ FootbagPix/Logic/IBallLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootbagPix.Logic
+{
+    public interface IBallLogic
+    {
+        void SetPosition();
+        void SetSpeed();
+    }
+}
---
+++ FootbagPix/Logic/ICharacterLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootbagPix.Logic
+{
+    public interface ICharacterLogic
+    {
+        void Move();
+        void TryHitBall();
+        void KickLeft();
+        void KickRight();
+        void Turn();
+    }
+}
