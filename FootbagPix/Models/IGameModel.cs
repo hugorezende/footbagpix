bec8eb89bd2c4d2cccd4bb5e1ce50f5237e30fa0
Krisztián Póra <krisztian.pora@gmail.com>
2020-03-29 15:12:07 +0200

    Added Models folder, IGameModel interface

---
+++ FootbagPix/Models/IGameModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootbagPix.Models
+{
+    public interface IGameModel
+    {
+        CharacterModel Character { get; set; }
+        BallModel Ball { get; set; }
+        string PlayerName { get; set; }
+        ScoreModel CurrentScore { get; set; }
+        TimeSpan ElapsedTime { get; set; }
+    }
+}
